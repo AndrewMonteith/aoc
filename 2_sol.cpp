8ecd1dba9e3cb2fe8b6383b49ac59331a1cc8eeb
AndrewMoteith <andrewmonteith1999@gmail.com>
2020-12-18 16:38:34 +0000

    day 2

---
+++ 2_sol.cpp
@@ -0,0 +1,50 @@
+#include "util.hpp"
+
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+struct PasswordPolicy {
+    int i1; int i2;
+    char letter;
+    std::string password;
+};
+
+PasswordPolicy parse(const std::string& line) {
+    std::stringstream stream(line);
+
+    int i1, i2;
+    stream >> i1;
+    stream.ignore(1) >> i2;
+
+    char letter;
+    stream >> letter;
+
+    std::string password;
+    stream.ignore(1) >> password;
+
+    return PasswordPolicy{i1, i2, letter, std::move(password)};
+}
+
+int main() {
+    namespace ranges = std::ranges;
+
+    auto p = util::parse("1_input", &parse);
+
+    const auto isValidDay1 = [](const PasswordPolicy& policy) {
+        const auto letterFreq = ranges::count(policy.password, policy.letter);
+        return policy.i1 <= letterFreq && letterFreq <= policy.i2;
+    };
+
+    const auto isValidDay2 = [](const PasswordPolicy& policy) {
+        const auto letter1 = policy.password[policy.i1-1];
+        const auto letter2 = policy.password[policy.i2-1];
+
+        return (letter1 == policy.letter && letter2 != policy.letter) ||
+                 (letter2 == policy.letter && letter1 != policy.letter);
+    };
+
+    std::cout << "Day 1: " << ranges::count_if(p, isValidDay1) << '\n';
+    std::cout << "Day 2: " << ranges::count_if(p, isValidDay2) << '\n';
+}
---
+++ util.hpp
@@ -0,0 +1,31 @@
+#ifndef INCLUDED_UTIL_H
+#define INCLUDED_UTIL_H
+
+#include <filesystem>
+#include <iterator>
+#include <fstream>
+#include <functional>
+#include <string>
+
+namespace util {
+
+template <typename Object>
+using Parser = Object (*)(const std::string&);
+
+template <typename Object, template <typename> typename Container=std::vector>
+Container<Object> parse(const std::string& fileName, Parser<Object> parser) {
+    auto container = Container<Object>();
+    auto inserter = std::back_inserter(container);
+
+    auto f = std::ifstream(std::filesystem::current_path() / fileName);
+    std::string line;
+    while (std::getline(f, line)) {
+        *inserter++ = parser(line);
+    }
+
+    return container;
+}
+
+};
+
+#endif
